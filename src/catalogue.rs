a79c1df237a5d2a6987a8bdf8e5a6a77530963c6
Chris <8133496+chriswmann@users.noreply.github.com>
2026-07-11 22:26:30 +0100

    Give Catalogue its index and entries() iterator

--- src/catalogue.rs
+++ src/catalogue.rs
@@ -1,2 +1,19 @@
+use crate::store::Index;
+
 #[derive(Debug)]
-pub struct Catalogue {}
+pub struct Catalogue {
+    index: Index,
+}
+
+impl Catalogue {
+    pub fn new(index: Index) -> Self {
+        Self { index }
+    }
+
+    pub fn entries(&self) -> impl Iterator<Item = (&str, &str)> {
+        self.index
+            .entries
+            .iter()
+            .map(|(p, e)| (p.as_str(), e.name.as_str()))
+    }
+}
