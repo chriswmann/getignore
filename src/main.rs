abc4975305df24718ab686bc8e0ed769d7abd9c1
Chris <8133496+chriswmann@users.noreply.github.com>
2026-09-09 22:50:53 +0100

    Add UI module

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,15 @@ mod github;
 mod option;
 mod resolve;
 mod store;
+mod ui;
 
 use error::AppError;
 use github::fetch_template;
 use option::Opts;
 use resolve::resolve_template_path;
 use store::unix_now;
+use ui::{display_app_error, display_template_error};
 
-use crate::error::TemplateError;
 use crate::{
     catalogue::Catalogue,
     store::{atomic_write_file, load_blob_from_cache, load_index, save_blob_to_cache},
@@ -140,39 +141,5 @@ fn should_proceed(path: impl AsRef<path::Path>) -> Result<bool, AppError> {
             "n" => return Ok(false),
             _ => {}
         }
-    }
-}
-
-fn display_app_error(app_error: &AppError) -> String {
-    match app_error {
-        AppError::HomeDir(err) => err.clone(),
-        AppError::DiskWrite(err) => format!("Could not write to disk: {err}"),
-        AppError::Io(err) => err.to_string(),
-        AppError::Network { context, source } => format!("Network error {context}, {source}"),
-        AppError::Time(err) => format!("There was an error reading the system time: {err}"),
-        AppError::Serialisation(err) => format!("There was an error parsing data: {err}"),
-        AppError::TruncatedTree => {
-            "The github git tree was truncated. Please try again".to_string()
-        }
-    }
-}
-
-fn display_template_error(error: &TemplateError) -> String {
-    match error {
-        TemplateError::DidYouMean { query, best, rest } => {
-            let mut buf: String =
-                format!("{query} did not match any templates. Did you mean {best}");
-            match rest.as_slice() {
-                [first] => buf.push_str(format!("or {first}").as_str()),
-                [first, second] => buf.push_str(format!(", {first} or {second}").as_str()),
-                _ => {}
-            }
-            buf.insert(buf.len(), '?');
-            buf
-        }
-        TemplateError::NotFound(query) => {
-            format!("Could not find any templates which matched {query}")
-        }
-        TemplateError::EmptyQuery => "Template query was empty".to_string(),
     }
 }
---
+++ src/ui.rs
@@ -0,0 +1,35 @@
+use crate::error::{AppError, TemplateError};
+
+pub fn display_app_error(app_error: &AppError) -> String {
+    match app_error {
+        AppError::HomeDir(err) => err.clone(),
+        AppError::DiskWrite(err) => format!("Could not write to disk: {err}"),
+        AppError::Io(err) => err.to_string(),
+        AppError::Network { context, source } => format!("Network error {context}, {source}"),
+        AppError::Time(err) => format!("There was an error reading the system time: {err}"),
+        AppError::Serialisation(err) => format!("There was an error parsing data: {err}"),
+        AppError::TruncatedTree => {
+            "The github git tree was truncated. Please try again".to_string()
+        }
+    }
+}
+
+pub fn display_template_error(error: &TemplateError) -> String {
+    match error {
+        TemplateError::DidYouMean { query, best, rest } => {
+            let mut buf: String =
+                format!("{query} did not match any templates. Did you mean {best}");
+            match rest.as_slice() {
+                [first] => buf.push_str(format!("or {first}").as_str()),
+                [first, second] => buf.push_str(format!(", {first} or {second}").as_str()),
+                _ => {}
+            }
+            buf.insert(buf.len(), '?');
+            buf
+        }
+        TemplateError::NotFound(query) => {
+            format!("Could not find any templates which matched {query}")
+        }
+        TemplateError::EmptyQuery => "Template query was empty".to_string(),
+    }
+}
